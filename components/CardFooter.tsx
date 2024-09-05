f101920b9579297b245b4b85d48ea1f7e4fb14b4
dom-inic <dominicnyambane22@gmail.com>
2024-09-05 19:53:32 +0300

    updates

--- components/CardFooter.tsx
+++ components/CardFooter.tsx
@@ -1,12 +1,16 @@
 import Image from "next/image";
 import Link from "next/link";
 import Github from "/public/github.svg";
-import Linkedin from "/public/linkedin.svg";
+import Pinterest from "/public/pinterest.svg";
+import Facebook from "/public/facebook.svg";
 import Twitter from "/public/twitter.svg";
+import Instagram from "/public/instagram.svg"
+
 const InstagramLink = process.env.NEXT_PUBLIC_INSTAGRAM || "https://www.instagram.com/coderfpv/";
 const TwitterLink = process.env.NEXT_PUBLIC_TWITTER || "https://www.x.com/coderfpv";
 const GithubLink = process.env.NEXT_PUBLIC_GITHUB || "https://www.github.com/coder-fpv";
 const FacebookLink = process.env.NEXT_PUBLIC_FACEBOOK || "https://www.facebook.com/coderfpv";
+const PinterestLink = process.env.NEXT_PUBLIC_PINTEREST || "https://www.facebook.com/coderfpv";
 
 const CardFooter = () => {
   return (
@@ -23,10 +27,16 @@ const CardFooter = () => {
       {/* Social Media */}
       <div className="flex justify-center items-center gap-2 mt-6">
         <Link href={InstagramLink} className="relative w-[22px] h-[22px]">
-          <Image src={Linkedin} alt="Instagram" fill />
+          <Image src={Instagram} alt="Instagram" fill />
+        </Link>
+        <Link href={FacebookLink} className="relative w-[22px] h-[22px]">
+          <Image src={Facebook} alt="Facebook" fill />
         </Link>
         <Link href={TwitterLink} className="relative w-[22px] h-[22px]">
           <Image src={Twitter} alt="Twitter" fill />
+        </Link>
+        <Link href={PinterestLink} className="relative w-[22px] h-[22px]">
+          <Image src={Pinterest} alt="pinterest" fill />
         </Link>
         <Link href={GithubLink} className="relative w-[22px] h-[22px]">
           <Image src={Github} alt="Github" fill />
